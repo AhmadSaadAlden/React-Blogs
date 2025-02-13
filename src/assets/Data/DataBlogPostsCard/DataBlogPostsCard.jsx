f78ded7aa194502899f6eda45ee3e55709305312
AhmadSaadAlden <ahmadsaad01045@gmail.com>
2025-02-14 02:00:18 +0300

    second commit

--- src/assets/Data/DataBlogPostsCard/DataBlogPostsCard.jsx
+++ src/assets/Data/DataBlogPostsCard/DataBlogPostsCard.jsx
@@ -2,7 +2,7 @@ import { FiArrowUpRight } from 'react-icons/fi';
 
 const DataBlogPostsCard = [
     {
-        img : "/assets/images/image1.png" , 
+        img : "assets/images/image1.png" , 
         alt : "Image1", 
         NameAndDateOfBirth : "Alec Whitten • 1 Jan 2023" ,
         title : "Bill Walsh leadership lessons", 
